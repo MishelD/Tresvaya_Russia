3a2a2292cb0d09c3323be59506b1e4b9f46e0330
Mihail <mishutkavgostyah@gmail.com>
2020-06-16 10:26:23 +0300

    add

---
+++ js/tgSwitch.js
@@ -0,0 +1,42 @@
+$('document').ready(function(){
+	$('.tgSwitch').each(function(){
+		if ($(this).find('input').attr('checked')) {
+			$(this).addClass('active');
+			$(this).find('.slider').css({"-webkit-transform" : "translateX(50px)"});
+		}
+	});
+});
+
+var checked = JSON.parse(localStorage.getItem('.themeStyle'));
+if (checked == true) {
+    document.getElementById(".themeStyle").checked = true;
+} 
+
+function save(){
+    var checkbox = document.querySelector('input');
+    localStorage.setItem('.themeStyle', checkbox.checked);
+}
+
+$('.check').click(
+function() {
+save();
+}
+)
+
+let theme = document.querySelector('.theme');
+
+$('.tgSwitch').click(function(){
+	if ($(this).find('input').attr('checked')) {
+		$(this).removeClass('active');
+		$(this).find('input').attr('checked', false);
+		$(this).find('.slider').css({"-webkit-transform" : "translateX(0px)"});
+		theme.textContent = 'светлая тема';
+		document.querySelector('.themeStyle').href="css/light.css";
+	} else {
+		$(this).addClass('active');
+		$(this).find('input').attr('checked', true);
+		$(this).find('.slider').css({"-webkit-transform" : "translateX(50px)"});
+		theme.textContent = 'тёмная тема';
+		document.querySelector('.themeStyle').href="css/dark.css";
+	}
+});
